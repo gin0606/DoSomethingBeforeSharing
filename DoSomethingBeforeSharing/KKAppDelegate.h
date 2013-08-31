be1f92dc2b8af4d4213d5bea55c82f42b412296d
gin0606 <k.kennichi.knlnshnm@gmail.com>
2013-08-31 22:20:41 +0900

    Initial Commit

---
+++ DoSomethingBeforeSharing/KKAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  KKAppDelegate.h
+//  DoSomethingBeforeSharing
+//
+//  Created by gin0606 on 2013/08/31.
+//  Copyright (c) 2013年 gin0606. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class KKViewController;
+
+@interface KKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) KKViewController *viewController;
+
+@end
---
+++ DoSomethingBeforeSharing/KKViewController.h
@@ -0,0 +1,13 @@
+//
+//  KKViewController.h
+//  DoSomethingBeforeSharing
+//
+//  Created by gin0606 on 2013/08/31.
+//  Copyright (c) 2013年 gin0606. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KKViewController : UIViewController
+
+@end
